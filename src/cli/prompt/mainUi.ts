098f1484da5d6fae06cf9119f644dc39ddd815c4
ghabxph <11975597+ghabxph@users.noreply.github.com>
2024-01-17 20:16:36 +0800

    feat(#36): be able to setup wallet

--- src/cli/prompt/mainUi.ts
+++ src/cli/prompt/mainUi.ts
@@ -5,7 +5,11 @@ import { utilUi } from "./utilUi";
 import { Yaml2Solana } from "../..";
 import { runInstructionUi } from "./runInstructionUi";
 import { Yaml2SolanaClass } from "../../sdk/Yaml2Solana";
+import { USER_WALLET, hasWallet, setupUserWalletUi, walletOptionsUi } from "./setupUserWalletUi";
+import clear from "ts-clear-screen";
 
+const CHOICE_SETUP_USER_WALLET = 'Setup a password-protected wallet in yaml2solana';
+const CHOICE_WALLET_OPTIONS = 'Wallet options';
 const DOWNLOAD_SOLANA_ACCOUNTS = 'Download solana accounts defined in schema';
 const CHOICE_RUN_TEST_VALIDATOR = 'Run test validator';
 const CHOICE_RUN_INSTRUCTION = 'Run an instruction';
@@ -13,61 +17,82 @@ const CHOICE_RUN_TEST = 'Run a test';
 const CHOICE_UTILS = 'Utility / Debugging Tools';
 
 export async function mainUi(schemaFile: string, y2s?: Yaml2SolanaClass) {
-  const { choice } = await inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'choice',
-        message: '-=Yaml2Solana Main UI=-\n\n [Choose Action]',
-        choices: [
-          DOWNLOAD_SOLANA_ACCOUNTS,
-          CHOICE_RUN_TEST_VALIDATOR,
-          CHOICE_RUN_INSTRUCTION,
-          CHOICE_RUN_TEST,
-          CHOICE_UTILS,
-        ],
-      },
-    ]
-  );
-
-  if (choice === DOWNLOAD_SOLANA_ACCOUNTS) {
-    await downloadSolanaAccounts(schemaFile, y2s);
-    return;
-  }
-
-  if (choice === CHOICE_RUN_TEST_VALIDATOR) {
-    await runTestValidator(schemaFile, y2s);
-    return;
-  }
-
-  // Assuming here that test validator is already running.
-  if (choice === CHOICE_RUN_INSTRUCTION) {
-    return await runInstructionUi(schemaFile, y2s);
-  }
-
-  // Assuming here that test validator is already running.
-  if (choice === CHOICE_RUN_TEST) {
-    // 1. Confirm first if localnet is running
-    if (!await util.test.checkIfLocalnetIsRunning()) {
-      console.log('Localnet seems not running. http://127.0.0.1:8899/health doesn\'t return a healthy status.');
+  let programLoop = [true];
+  while (programLoop[0]) {
+    clear();
+    let walletState: string = '';
+    if (hasWallet()) {
+      if (USER_WALLET === undefined) {
+        walletState = 'User wallet state: Locked\n\n';
+      } else {
+        walletState = 
+          'User wallet state: Unlocked\n' +
+          `Wallet address: ${USER_WALLET.publicKey}\n\n`;
+      }
+    }
+    const { choice } = await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'choice',
+          message: `${walletState}-=Yaml2Solana Main UI=-\n\n [Choose Action] \n\n`,
+          choices: [
+            hasWallet() ? CHOICE_WALLET_OPTIONS : CHOICE_SETUP_USER_WALLET,
+            DOWNLOAD_SOLANA_ACCOUNTS,
+            CHOICE_RUN_TEST_VALIDATOR,
+            CHOICE_RUN_INSTRUCTION,
+            CHOICE_RUN_TEST,
+            CHOICE_UTILS,
+          ],
+        },
+      ]
+    );
+  
+    if (choice === CHOICE_SETUP_USER_WALLET) {
+      await setupUserWalletUi();
+    }
+  
+    if (choice === CHOICE_WALLET_OPTIONS) {
+      await walletOptionsUi();
+    }
+  
+    if (choice === DOWNLOAD_SOLANA_ACCOUNTS) {
+      return await downloadSolanaAccounts(schemaFile, y2s);
+    }
+  
+    if (choice === CHOICE_RUN_TEST_VALIDATOR) {
+      return await runTestValidator(schemaFile, y2s);
+    }
+  
+    // Assuming here that test validator is already running.
+    if (choice === CHOICE_RUN_INSTRUCTION) {
+      return await runInstructionUi(schemaFile, y2s);
+    }
+  
+    // Assuming here that test validator is already running.
+    if (choice === CHOICE_RUN_TEST) {
+      // 1. Confirm first if localnet is running
+      if (!await util.test.checkIfLocalnetIsRunning()) {
+        console.log('Localnet seems not running. http://127.0.0.1:8899/health doesn\'t return a healthy status.');
+        return;
+      }
+  
+      // Confirm messae that localnet is running
+      console.log('Localnet seems running. http://127.0.0.1:8899/health returns \'ok\' state');
+  
+      // 2. Select what test to execute
+      // 3. Check if there are missing parameters
+      // 4. Run test validator
+      // 5. Run instruction from test
+  
       return;
     }
-
-    // Confirm messae that localnet is running
-    console.log('Localnet seems running. http://127.0.0.1:8899/health returns \'ok\' state');
-
-    // 2. Select what test to execute
-    // 3. Check if there are missing parameters
-    // 4. Run test validator
-    // 5. Run instruction from test
-
-    return;
-  }
-
-  // Debugging Tools
-  if (choice === CHOICE_UTILS) {
-    await utilUi(schemaFile, y2s);
-    return;
+  
+    // Debugging Tools
+    if (choice === CHOICE_UTILS) {
+      await utilUi(schemaFile, y2s);
+      return;
+    }
   }
 }
 
---
+++ src/cli/prompt/setupUserWalletUi.ts
@@ -0,0 +1,125 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+import inquirer from "inquirer";
+import * as web3 from '@solana/web3.js';
+import * as bip39 from 'bip39';
+import { AES, enc } from "crypto-ts";
+import { derivePath } from 'ed25519-hd-key';
+
+const CHOICE_GENERATE_RANDOM = 'Generate a random wallet';
+const CHOICE_GENERATE_FROM_BIP39 = 'Generate from bip39 passphrase';
+const keyPath = path.resolve(os.homedir(), '.wallet-yaml2solana.dat');
+export let USER_WALLET: undefined | web3.Keypair;
+
+export async function setupUserWalletUi() {
+  const { password } = await inquirer.prompt({
+    type: 'password',
+    name: 'password',
+    message: 'Enter password to secure your wallet:'
+  });
+  const { confirmPassword } = await inquirer.prompt({
+    type: 'password',
+    name: 'confirmPassword',
+    message: 'Confirm:'
+  });
+  if (password !== confirmPassword) {
+    console.log('Password does not match!');
+    return;
+  }
+  const { choice } = await inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'Choose action',
+    choices: [
+      CHOICE_GENERATE_RANDOM,
+      CHOICE_GENERATE_FROM_BIP39,
+    ]
+  });
+
+  if (choice === CHOICE_GENERATE_RANDOM) {
+    await generateRandomWallet(keyPath, password)
+  } else if (choice === CHOICE_GENERATE_FROM_BIP39) {
+    await generateFromBip39Wallet(keyPath, password);
+  }
+}
+
+async function generateRandomWallet(keyPath: string, password: string) {
+  const kp = web3.Keypair.generate();
+  const secret = Buffer.from(kp.secretKey).toString('base64');
+  const encrypted = AES.encrypt(secret, password).toString();
+  fs.writeFileSync(keyPath, encrypted);
+  console.log('Your public key: ', kp.publicKey.toBase58());
+  console.log(`Created ${keyPath}\nRun yaml2solana program again!\n\n`);
+}
+
+async function generateFromBip39Wallet(keyPath: string, password: string) {
+  const { seedPhrase } = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'seedPhrase',
+        message: 'Type 24-word seephrase here:'
+      }
+    ]);
+
+  const seed = bip39.mnemonicToSeedSync(seedPhrase);
+  const seedBuffer = Buffer.from(seed).toString('hex');
+  const derivationPath = `m/44'/501'/0'/0'`; // TODO: Be able to choose derivation path
+  const deriveSeed = derivePath(derivationPath, seedBuffer).key;
+  const kp = web3.Keypair.fromSeed(deriveSeed);
+  const secret = Buffer.from(kp.secretKey).toString('base64');
+  const e = AES.encrypt(secret, password);
+  const encrypted = e.toString();
+  fs.writeFileSync(keyPath, encrypted);
+
+  console.log(`Derivation path: ${derivationPath}`);
+  console.log(`Your public key: ${kp.publicKey}`);
+  console.log(`Created ${keyPath}\nRun yaml2solana program again!\n\n`);
+}
+
+export function hasWallet(): boolean {
+  try {
+    fs.readFileSync(keyPath);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+const CHOICE_REMOVE_WALLET = 'Remove user wallet';
+const CHOICE_UNLOCK_WALLET = 'Unlock wallet so that you can use it';
+
+export async function walletOptionsUi() {
+  const { choice } = await inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'Wallet options:',
+    choices: [
+      CHOICE_UNLOCK_WALLET,
+      CHOICE_REMOVE_WALLET,
+    ]
+  });
+
+  if (choice === CHOICE_REMOVE_WALLET) {
+    fs.unlinkSync(keyPath);
+    console.log(`Key ${keyPath} has been removed successfully.`);
+  } else if (choice === CHOICE_UNLOCK_WALLET) {
+    const { password } = await inquirer.prompt({
+      type: 'password',
+      name: 'password',
+      message: 'Enter password to unlock wallet:'
+    });
+    const encrypted = fs.readFileSync(keyPath).toString('utf-8');
+    const wallet = AES.decrypt(encrypted, password).toString(enc.Utf8);
+    try {
+      const kp = web3.Keypair.fromSecretKey(Buffer.from(wallet, 'base64'));
+      console.log(`Your public key: ${kp.publicKey}`);
+      console.log(`Wallet has been unlocked successfully!`);
+      USER_WALLET = kp;
+    } catch {
+      console.log('Invalid password!');
+      process.exit(-1);
+    }
+  }
+}
